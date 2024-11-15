d90798055b29586b1c7a19cea5c941d5516567a9
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2024-11-15 15:13:47 -0700

    Further cleaning of StaticVector interface

--- src/_Species.jl
+++ src/_Species.jl
@@ -5,27 +5,34 @@ using StaticArrays
 
 struct Species{L,T,N} <: StaticVector{N,T}
     data :: SVector{N,T}
-    Species{L,T,N}(x::Tuple) where {L,T,N}    = new{L,T,length(L)}(SVector{length(L),T}(x))
-    Species{L,T,N}(x::SVector) where {L,T,N}  = new{L,T,length(L)}(SVector{length(L),T}(x))
-    Species{L,T}(x::Tuple)   where {L,T}      = new{L,T,length(L)}(SVector{length(L),T}(x))
-    Species{L,T}(x::SVector) where {L,T}      = new{L,T,length(L)}(SVector{length(L),T}(x))
-    Species{L}(x::SVector{N,T}) where {L,T,N} = new{L,T,length(L)}(SVector{length(L),T}(x))
+    Species{L,T,N}(x::Tuple)  where {L,T,N}  = new{L, T, length(L)}(SVector{length(L),T}(x))
+    Species{L,T}(x::Tuple)    where {L,T}    = new{L, T, length(L)}(SVector{length(L),T}(x)) 
+    function Species{L}(x::Tuple) where {L}  
+        T = Base.promote_typeof(x...)
+        return new{L, T, length(L)}(SVector{length(L),T}(x))
+    end
 end
 
-Species{L}(x::Tuple) where {L} = Species{L}(SVector{length(L)}(x))
+#Extend key StaticArray interface functions
+StaticArrays.length(::Type{<:Species{L}}) where L = length(L)
+StaticArrays.construct_type(::Type{Species{L,T}}, x) where {L,T} = StaticArrays.construct_type(Species{L,T,length(L)}, x)
+StaticArrays.construct_type(::Type{Species{L}}, x::AbstractArray{T}) where {L,T} = StaticArrays.construct_type(Species{L,T,length(L)}, x)
+StaticArrays.construct_type(::Type{Species{L}}, x::Tuple) where {L} = StaticArrays.construct_type(Species{L,Base.promote_typeof(x...),length(L)}, x)
 
+#Add support to NamedTuples and keyword arguments
 Species{L,T}(x::NamedTuple) where {L,T} = Species{L,T}(values(x[L]))
-Species{L}(x::NamedTuple) where {L}     = Species{L}(values(x[L]))
-Species{L,T}(;kwargs...) where {L,T}    = Species{L,T}(kwargs[L])
+Species{L}(x::NamedTuple)  where {L}    = Species{L}(values(x[L]))
+Species{L,T}(;kwargs...)  where {L,T}   = Species{L,T,N}(kwargs[L])
 Species{L}(;kwargs...) where {L}        = Species{L}(kwargs[L])
 
+#Indexing functions
 Base.getindex(x::Species, i::Int) = x.data[i]
-
 function Base.getindex(x::Species{L,T,N}, i::Symbol) where {L,T,N}
     nt = NamedTuple{L}(Base.OneTo(length(L)))
     return x.data[nt[i]]
 end
 
+#Introspection
 species(::Type{Species{L,T,N}}) where {L,T,N} = L
 species(x::Type{Species{L,T,N}}) where {L,T,N} = L
 Base.propertynames(x::Type{Species{L,T,N}}) where {L,T,N} = L
