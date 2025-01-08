b4236dc6412fb2036e31d35a21bd480c4130b35a
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-01-07 17:54:30 -0700

    Update todo list

--- test/co2_capture_test.jl
+++ test/co2_capture_test.jl
@@ -2,6 +2,23 @@ using Revise
 using MassBalanceReconciler
 using JSON3
 
+#===========================================================================================================
+ToDo:
+
+(1) Ensure that the dictionary has a "_UNIX_TIMESTAMP" entry
+(2) Remove the "timestamp" argument from readvalues() and use "_UNIX_TIMESTAMP" instead
+(3)  -  Ensure that the evaluation interval is properly calculated and updated for all balance measurements
+(4) Wrap entire procedure into a reconcile!(plantstate, measdict) call
+     -  readvalues!
+     -  updatethermo!
+     -  predict!
+     -  reconcile_statevec!
+     -  reconcile_statecov!
+     -  update_balance_errors!
+
+===========================================================================================================#
+
+
 capture_dict = open(joinpath(@__DIR__,"co2_capture_plant.json")) do fh
     JSON3.read(fh, numbertype=Float64)
 end
