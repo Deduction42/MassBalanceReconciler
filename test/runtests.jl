da9d7e52be930c17b0f0f4b39bc969359fd002e0
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2025-02-18 08:39:17 -0700

    Added some test run scripts for later use

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using MassBalanceReconciler
 using Test
 
+#For coverage, run with
+#julia --startup-file=no --depwarn=yes --threads=auto -e 'using Coverage; clean_folder(\"src\"); clean_folder(\"test\"); clean_folder(\"ext\")'
+#julia --startup-file=no --depwarn=yes --threads=auto --code-coverage=user --project=. -e 'using Pkg; Pkg.test(coverage=true)'
+#julia --startup-file=no --depwarn=yes --threads=auto coverage.jl
+
 @testset "MassBalanceReconciler.jl" begin
     # Write your tests here.
 end
